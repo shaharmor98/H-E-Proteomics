29a7ea40aa26472dc3f9933d8d356f67caf2fcdc
Shahar Mor <shahar.mor@Shahars-Macbook.CS.Technion.ac.il>
2023-01-26 11:51:23 +0200

    bugfix

--- host_configuration.py
+++ host_configuration.py
@@ -6,7 +6,7 @@ class HostConfiguration(object):
     SLIDES_DIRECTORY = "../data/slides"
     TILES_DIRECTORY = os.path.join(os.path.expanduser("~"), "images/zoom_{zoom_level}_size_{patch_size}")
     RNR_METADATA_FILE_PATH = os.path.join(os.path.expanduser("~"), "proteomics_from_janne", "Labels_etc",
-                                          "Labels_etc_230109_RNr_to_metadata.xlsx")
+                                          "230109_RNr_to_metadata.xlsx")
     DIA_GENES_FILE_PATH = os.path.join(os.path.expanduser("~"), "proteomics_from_janne", "Labels_etc",
                                        "SCANB_TNBC_diaPASEF_SN16_E104.xlsx")
     PATCH_SIZE = 512
