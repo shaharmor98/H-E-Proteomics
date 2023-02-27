f0be6feabfab99e8d5f07135453edcd33097a258
Shahar Mor <shahar.mor@Shahars-Macbook.lan>
2023-02-27 12:42:08 +0200

    add some code

--- models/pytorch/protein_quant_predictor.py
+++ models/pytorch/protein_quant_predictor.py
@@ -38,6 +38,8 @@ class ProteinQuantPredictor(pl.LightningModule):
     def forward(self, x):
         img, features = x
         image_features = self.model(img)
+        print("Img features shape: ", image_features.shape)
+        print("features shape: ", features.shape)
         x = torch.concatenate([image_features, features], dim=1)
         print("X, shape: ", x.shape)
         x = F.relu(self.fc1(x))
