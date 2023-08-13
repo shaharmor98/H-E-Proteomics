5d7de9b9dc34c054c7f5df42739f0802ea3e99b1
Shahar Mor <shahar.mor@Shahars-Macbook.lan>
2023-08-13 13:48:17 +0300

    add code

--- models/SOLTI_like_pipeline/main.py
+++ models/SOLTI_like_pipeline/main.py
@@ -85,14 +85,10 @@ def train(args, gene):
                                                                          val_proportion=0.35)
         model = ProteinQuantClassifier(device).to(device)
         wandb_logger = WandbLogger(project="proteomics-project", log_model=True,
-                                   save_dir=Configuration.CHECKPOINTS_PATH.format(gene=gene),
-                                   checkpoint_name=gene + "-round-" + str(n_round))
+                                   save_dir=Configuration.CHECKPOINTS_PATH.format(gene=gene))
         trainer = pl.Trainer(max_epochs=10, devices="auto", accelerator="auto",
-                             num_sanity_val_steps=0, logger=wandb_logger, strategy="ddp", max_steps=2,
-                             )  # todo- return callback!!!
-        # callbacks=[EarlyStopping(monitor="val_epoch_loss", patience=5, mode="min")])
-        # default_root_dir=Configuration.CHECKPOINTS_PATH.format(
-        #     gene=gene + "-round-" + str(n_round)))
+                             num_sanity_val_steps=0, logger=wandb_logger, strategy="ddp",
+                             callbacks=[EarlyStopping(monitor="val_epoch_loss", patience=5, mode="min")])
         train_dataset = TilesDataset(tiles_directory_path, transform_compose, train_instances, "Train-dataset")
         validation_dataset = TilesDataset(tiles_directory_path, transform_compose, valid_instances, "Val-dataset")
 
